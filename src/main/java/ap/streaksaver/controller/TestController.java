9c0f8ac80fa5b9271010df0e1c678dec68dc932c
alessandro.pinna <MjYyNDcyNDM5NDA2OqlTdEqfJbFAUeI9YPE94SeDPNsb>
2021-08-29 22:28:56 +0200

    Removed unused code

--- src/main/java/ap/streaksaver/controller/TestController.java
+++ src/main/java/ap/streaksaver/controller/TestController.java
@@ -1,10 +1,6 @@
 package ap.streaksaver.controller;
 
-import ap.streaksaver.repository.UserRepository;
-import ap.streaksaver.service.LoginService;
-import ap.streaksaver.utils.PasswordUtils;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -12,48 +8,20 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/test")
 class TestController {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private LoginService loginService;
-
-    @Autowired
     JobLauncher jobLauncher;
 
     @Autowired
     Job job;
-
-    @GetMapping("/hash")
-    public ResponseEntity<String> hash(@RequestParam String password) {
-        try {
-            return ResponseEntity.ok(PasswordUtils.hashNewPassword(password).getSecond());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Hashing error");
-        }
-    }
-
-    @GetMapping("/login")
-    public ResponseEntity login(@RequestParam String identifier, @RequestParam String password) throws NoSuchAlgorithmException {
-
-        ResponseEntity responseEntity = loginService.doLogin(identifier, password);
-
-        return responseEntity;
-    }
 
     @Scheduled(cron="${batch.cron-exp}")
     @GetMapping("/batch")
