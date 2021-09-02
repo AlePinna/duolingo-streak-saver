a07f105d35ebb49bf784c77bab2c183a0903497b
alessandro.pinna <MjYyNDcyNDM5NDA2OqlTdEqfJbFAUeI9YPE94SeDPNsb>
2021-09-03 01:48:27 +0200

    Fix scripts not being extracted

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /tmp/streaksaver-1.0.0.jar &> /dev/null
+java -jar /tmp/streaksaver-1.0.0.jar > /dev/null 2> /dev/null < /dev/null &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -ef | grep streakfreeze-1-0-0.jar | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep streakfreeze-1-0-0.jar | grep -v grep | awk '{print $2}' | xargs kill &
