ca9b09ead44475544bec7c95d0627e9a864d6644
alessandro.pinna <MjYyNDcyNDM5NDA2OqlTdEqfJbFAUeI9YPE94SeDPNsb>
2021-09-04 01:17:44 +0200

    Updated scripts to start mongo

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /home/ec2-user/streaksaver-1.0.0.jar &> streaksaver.log &
+service mongod start & java -jar /home/ec2-user/streaksaver-1.0.0.jar &> streaksaver.log &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -ef | grep streaksaver | grep -v grep | awk '{print $2}' | xargs kill -9 &> kill.txt &
+ps -ef | grep streaksaver | grep -v grep | awk '{print $2}' | xargs kill -9 &> kill.txt & rm -rf /tmp/mongodb-27017.sock &
