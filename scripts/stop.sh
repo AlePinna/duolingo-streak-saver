8ca3d8407577976c23011a0cb275a043276e5dbd
alessandro.pinna <MjYyNDcyNDM5NDA2OqlTdEqfJbFAUeI9YPE94SeDPNsb>
2021-09-04 00:57:00 +0200

    Removed directory test

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pwd &> directory.txt & ps -ef | grep streaksaver | grep -v grep | awk '{print $2}' | xargs kill -9 &> kill.txt &
+ps -ef | grep streaksaver | grep -v grep | awk '{print $2}' | xargs kill -9 &> kill.txt &
