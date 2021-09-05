2547c425b6baf023e8039d1efa8ffb339c59ed62
alessandro.pinna <MjYyNDcyNDM5NDA2OqlTdEqfJbFAUeI9YPE94SeDPNsb>
2021-09-05 03:24:52 +0200

    Api info

--- src/main/java/ap/streaksaver/config/SpringFoxConfig.java
+++ src/main/java/ap/streaksaver/config/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package ap.streaksaver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +18,7 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .paths(PathSelectors.any())
                 .apis(RequestHandlerSelectors.any())
@@ -22,7 +26,16 @@ public class SpringFoxConfig {
     }
 
     @Bean
-    RestTemplate getRestTemplate(){
+    RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Streaksaver")
+                .contact(new Contact("Alessandro Pinna", null, "alessandro.pinna95@gmail.com"))
+                .license("MIT")
+                .licenseUrl("https://opensource.org/licenses/MIT")
+                .build();
     }
 }
