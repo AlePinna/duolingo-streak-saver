d690b7e01ae3d1fc4f2d73e6bce8770cbe30ee2e
AlePinna <alessandro.pinna95@gmail.com>
2021-11-24 00:39:01 +0100

    Changed logging path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-service mongod start & java -jar /home/ec2-user/streaksaver-1.0.0.jar &> streaksaver.log &
+service mongod start & java -jar /home/ec2-user/streaksaver-1.0.0.jar &>> /home/ec2-user/streaksaver.log &
